189fda46b9e40d003da8b7a68fdebc1b94540fec
Niclas Älmeby <niclas.almeby@hotmail.com>
2020-06-02 14:36:35 +0200

    Update BTSelectHidingSpot.cs

--- AI_Testing_xNode/Assets/Scripts/BTSelectHidingSpot.cs
+++ AI_Testing_xNode/Assets/Scripts/BTSelectHidingSpot.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 [BTAgent(typeof(BTSelectHidingSpot))]
@@ -8,51 +10,59 @@ public class BTSelectHidingSpot : BTNode
     int totalsum = 0;
     public override BTResult Execute()
     {
-        
+        GetRandom();
+    }
 
 
-        foreach(HidingSpot spot in context.contextOwner.internalHidingSpots)
+    // Hämtar ett gömställe utifrån gömställenas sanorlikhet att bli vald
+    public HidingSpot GetRandom()
+    {
+        foreach (HidingSpot spot in context.contextOwner.internalHidingSpots)
         {
             totalsum += spot.probability;
         }
 
+        // Hämtar ett slumpmässing värde från 0 till totalsum-1
+        int index = UnityEngine.Random.Range(0, totalsum);
+        int sum = 0;
+        int i = 0;
 
-        throw new System.NotImplementedException();
-    }
+        while(sum < index)
+        {
+            sum += context.contextOwner.internalHidingSpots[i].probability;
+            i++;
+        }
 
-    public HidingSpot GetRandom()
-    {
+        return context.contextOwner.internalHidingSpots[i];
 
-        int index = Random.Range(0, totalsum);
-        
     }
 }
 
 
 
-class RandomSelector
-{
-    List<Item> items = new List();
-    Random rand = new Random();
-    int totalSum = 0;
+//class RandomSelector
+//{
+//    List<Item> items = new List();
+//    Random rand = new Random();
+//    int totalSum = 0;
 
-    RandomSelector()
-    {
-        for (Item item : items)
-        {
-            totalSum = totalSum + item.relativeProb;
-        }
-    }
+//    RandomSelector()
+//    {
+//        for (Item item : items)
+//        {
+//            totalSum = totalSum + item.relativeProb;
+//        }
+//    }
 
-    public Item getRandom()
-    {
+//    public Item getRandom()
+//    {
 
-        int index = rand.nextInt(totalSum);
-        int sum = 0;
-        int i = 0;
-        while (sum < index)
-        {
-            sum = sum + items.get(i++).relativeProb;
-        }
-        return items.get(Math.max(0, i - 1));
-    }
+//        int index = rand.nextInt(totalSum);
+//        int sum = 0;
+//        int i = 0;
+//        while (sum < index)
+//        {
+//            sum = sum + items.get(i++).relativeProb;
+//        }
+//        return items.get(Math.max(0, i - 1));
+//    }
