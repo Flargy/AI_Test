9b83149276f074f0a280186107da36076a008b86
Flargy <marlun1996@hotmail.com>
2020-06-03 11:51:24 +0200

    git ignore changes

--- AI_Testing_xNode/Assets/Scripts/PlayerScript.cs
+++ AI_Testing_xNode/Assets/Scripts/PlayerScript.cs
@@ -6,23 +6,14 @@ using UnityEngine.AI;
 public class PlayerScript : MonoBehaviour
 {
 
-    NavMeshAgent agent;
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
     }
 
     
     void Update()
     {
-
-        if (agent.hasPath == false)
-        {
-            Vector3 movement = new Vector3(15, 0, 0);
-            agent.SetDestination(agent.gameObject.transform.position + movement);
-        }
-
 
 
     }
